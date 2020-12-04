2e0fa0f94535bb720216b6f58fc465a5f1fdf5f7
Max Carlquist <tenkaklet@gmail.com>
2020-12-04 15:53:36 +0100

    la till post

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,20 @@ var router = express.Router();
 const faker = require('faker');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
   const data = [];
   for (let i = 0; i < 20; i++) {
     data.push(faker.name.firstName());
   }
   res.json(data);
+});
+
+
+router.post('/:name', (req, res, next) => {
+  const name = req.params.name;
+  res.json({
+    message: 'Grattis du la till namnet ' + name
+  })
 });
 
 module.exports = router;
